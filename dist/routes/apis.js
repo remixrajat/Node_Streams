10572be6ff0f1757875c2b78d8a67f373ef991c3
Rajat <remixrajat1@gmail.com>
2022-06-24 17:57:35 +0530

    another approach to read the data line by line

--- dist/routes/apis.js
+++ dist/routes/apis.js
@@ -8,6 +8,13 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __asyncValues = (this && this.__asyncValues) || function (o) {
+    if (!Symbol.asyncIterator) throw new TypeError("Symbol.asyncIterator is not defined.");
+    var m = o[Symbol.asyncIterator], i;
+    return m ? m.call(o) : (o = typeof __values === "function" ? __values(o) : o[Symbol.iterator](), i = {}, verb("next"), verb("throw"), verb("return"), i[Symbol.asyncIterator] = function () { return this; }, i);
+    function verb(n) { i[n] = o[n] && function (v) { return new Promise(function (resolve, reject) { v = o[n](v), settle(resolve, reject, v.done, v.value); }); }; }
+    function settle(resolve, reject, d, v) { Promise.resolve(v).then(function(v) { resolve({ value: v, done: d }); }, reject); }
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -17,47 +24,74 @@ const fs_1 = __importDefault(require("fs"));
 const axios_1 = __importDefault(require("axios"));
 const zlib = require("zlib");
 const es = require("event-stream");
+const readline = require("readline");
 const node_stream_1 = require("node:stream");
-const node_stream_2 = require("node:stream");
 const router = (0, express_1.Router)();
 router.get("/readMultiple", (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const stream = fs_1.default.createReadStream(`.././Node_Streams/data.json`, {
-            highWaterMark: 30,
-        });
-        // stream.pipe(res);
-        let data = "";
-        stream.on("data", (chunk) => {
-            data += chunk;
-        });
-        const getDataValue = new Promise((resolve) => {
-            stream.on("end", function () {
-                resolve(data);
-            });
-        });
-        let finalData = yield getDataValue;
-        let parsedObj = JSON.parse(finalData);
-        function getKey(key) {
-            return `${key}`;
-        }
-        function* run() {
-            for (const item in parsedObj) {
-                let sendableIte = {
-                    [getKey(item)]: parsedObj[item],
-                };
-                yield sendableIte;
-            }
-        }
-        const readableStream = new node_stream_1.Readable({
-            read() {
-                for (const data of run()) {
-                    this.push(JSON.stringify(data).concat("\n"));
+        const fileStream = fs_1.default.createReadStream(`.././Node_Streams/data.json`);
+        function processLineByLine() {
+            var e_1, _a;
+            return __awaiter(this, void 0, void 0, function* () {
+                const newfileStream = fs_1.default.createReadStream(`.././Node_Streams/data.json`);
+                const rl = readline.createInterface({
+                    input: newfileStream,
+                    crlfDelay: Infinity,
+                });
+                try {
+                    for (var rl_1 = __asyncValues(rl), rl_1_1; rl_1_1 = yield rl_1.next(), !rl_1_1.done;) {
+                        const line = rl_1_1.value;
+                        // console.log(`Line from file: ${line}`);
+                        fileStream.push(line);
+                    }
                 }
-                // just saying that the stream has finished!
-                this.push(null);
-            },
-        });
-        readableStream.pipe(res);
+                catch (e_1_1) { e_1 = { error: e_1_1 }; }
+                finally {
+                    try {
+                        if (rl_1_1 && !rl_1_1.done && (_a = rl_1.return)) yield _a.call(rl_1);
+                    }
+                    finally { if (e_1) throw e_1.error; }
+                }
+                fileStream.push(null);
+            });
+        }
+        processLineByLine();
+        fileStream.pipe(res);
+        // const stream = fs.createReadStream(`.././Node_Streams/data.json`, {
+        //   highWaterMark: 30,
+        // });
+        // // stream.pipe(res);
+        // let data = "";
+        // stream.on("data", (chunk) => {
+        //   data += chunk;
+        // });
+        // const getDataValue = new Promise((resolve) => {
+        //   stream.on("end", function () {
+        //     resolve(data);
+        //   });
+        // });
+        // let finalData: any = await getDataValue;
+        // let parsedObj = JSON.parse(finalData);
+        // function getKey(key: any) {
+        //   return `${key}`;
+        // }
+        // function* run() {
+        //   for (const item in parsedObj) {
+        //     let sendableIte = {
+        //       [getKey(item)]: parsedObj[item],
+        //     };
+        //     yield sendableIte;
+        //   }
+        // }
+        // const readableStream = new Readable({
+        //   read() {
+        //     for (const data of run()) {
+        //       this.push(JSON.stringify(data).concat("\n"));
+        //     }
+        //     this.push(null);
+        //   },
+        // });
+        // readableStream.pipe(res);
     }
     catch (error) {
         console.log(error);
@@ -76,18 +110,19 @@ router.get("/writeMultiple", (req, res, next) => __awaiter(void 0, void 0, void 
         }).then(function (response) {
             // let i = 1;
             response.data
-                .pipe(new node_stream_2.Transform({
+                .pipe(new node_stream_1.Transform({
                 objectMode: true,
                 transform(chunk, enc, cb) {
                     cb(null, chunk);
                 },
             }))
-                .pipe(new node_stream_2.Writable({
+                .pipe(new node_stream_1.Writable({
                 objectMode: true,
                 write(chunk, enc, cb) {
                     count++;
-                    // console.log(chunk);
-                    d += chunk.toString();
+                    // console.log(chunk.toString(), "uuuuuuuuuuuuuuu");
+                    if (!chunk.toString().startsWith("{"))
+                        d += chunk.toString();
                     const writeStream = fs_1.default.createWriteStream(`.././Node_Streams/email${i}.txt`, {
                         flags: "a",
                     });
--- src/routes/apis.ts
+++ src/routes/apis.ts
@@ -3,53 +3,66 @@ import fs from "fs";
 import axios from "axios";
 const zlib = require("zlib");
 const es = require("event-stream");
+const readline = require("readline");
 import { Readable } from "node:stream";
 import { Transform, Writable } from "node:stream";
 const router = Router();
 
 router.get("/readMultiple", async (req: any, res: any, next) => {
   try {
-    const stream = fs.createReadStream(`.././Node_Streams/data.json`, {
-      highWaterMark: 30,
-    });
-    // stream.pipe(res);
-    let data = "";
-    stream.on("data", (chunk) => {
-      data += chunk;
-    });
-
-    const getDataValue = new Promise((resolve) => {
-      stream.on("end", function () {
-        resolve(data);
+    const fileStream = fs.createReadStream(`.././Node_Streams/data.json`);
+    async function processLineByLine() {
+      const newfileStream = fs.createReadStream(`.././Node_Streams/data.json`);
+      const rl = readline.createInterface({
+        input: newfileStream,
+        crlfDelay: Infinity,
       });
-    });
-    let finalData: any = await getDataValue;
-
-    let parsedObj = JSON.parse(finalData);
-
-    function getKey(key: any) {
-      return `${key}`;
-    }
-    function* run() {
-      for (const item in parsedObj) {
-        let sendableIte = {
-          [getKey(item)]: parsedObj[item],
-        };
-        yield sendableIte;
+      for await (const line of rl) {
+        // console.log(`Line from file: ${line}`);
+        fileStream.push(line);
       }
+
+      fileStream.push(null);
     }
 
-    const readableStream = new Readable({
-      read() {
-        for (const data of run()) {
-          this.push(JSON.stringify(data).concat("\n"));
-        }
-        // just saying that the stream has finished!
-        this.push(null);
-      },
-    });
+    processLineByLine();
+    fileStream.pipe(res);
 
-    readableStream.pipe(res);
+    // const stream = fs.createReadStream(`.././Node_Streams/data.json`, {
+    //   highWaterMark: 30,
+    // });
+    // // stream.pipe(res);
+    // let data = "";
+    // stream.on("data", (chunk) => {
+    //   data += chunk;
+    // });
+    // const getDataValue = new Promise((resolve) => {
+    //   stream.on("end", function () {
+    //     resolve(data);
+    //   });
+    // });
+    // let finalData: any = await getDataValue;
+    // let parsedObj = JSON.parse(finalData);
+    // function getKey(key: any) {
+    //   return `${key}`;
+    // }
+    // function* run() {
+    //   for (const item in parsedObj) {
+    //     let sendableIte = {
+    //       [getKey(item)]: parsedObj[item],
+    //     };
+    //     yield sendableIte;
+    //   }
+    // }
+    // const readableStream = new Readable({
+    //   read() {
+    //     for (const data of run()) {
+    //       this.push(JSON.stringify(data).concat("\n"));
+    //     }
+    //     this.push(null);
+    //   },
+    // });
+    // readableStream.pipe(res);
   } catch (error: any) {
     console.log(error);
     res.status(400).json({ message: error.message });
@@ -82,8 +95,8 @@ router.get("/writeMultiple", async (req: any, res: any, next) => {
             objectMode: true,
             write(chunk, enc, cb) {
               count++;
-              // console.log(chunk);
-              d += chunk.toString();
+              // console.log(chunk.toString(), "uuuuuuuuuuuuuuu");
+              if (!chunk.toString().startsWith("{")) d += chunk.toString();
               const writeStream = fs.createWriteStream(
                 `.././Node_Streams/email${i}.txt`,
                 {
