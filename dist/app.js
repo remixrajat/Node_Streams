c781281ec80ca439eff6617319eeb22d1d5d9cfa
Rajat <remixrajat1@gmail.com>
2022-06-24 16:51:21 +0530

    updated api

--- dist/app.js
+++ dist/app.js
@@ -5,14 +5,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 // const express = require("express");
 const express_1 = __importDefault(require("express"));
-const todos_1 = __importDefault(require("./routes/todos"));
+const apis_1 = __importDefault(require("./routes/apis"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const fs_1 = __importDefault(require("fs"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use(body_parser_1.default.json());
-app.use(todos_1.default);
+app.use(apis_1.default);
 app.use("/", (req, res) => {
     const stream = fs_1.default.createReadStream(`${__dirname}/data.txt`);
     stream.pipe(res);
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 // const express = require("express");
 import express from "express";
-import todosRouter from "./routes/todos";
+import todosRouter from "./routes/apis";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import fs from "fs";
